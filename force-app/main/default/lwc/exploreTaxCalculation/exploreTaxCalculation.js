9448cd67554e466ba1e6e48ad214d0550fa0aca4
venkat <venkatanagarajas@gmail.com>
2022-03-12 22:35:35 +0530

    calling child function from a parent changes

--- force-app/main/default/lwc/exploreTaxCalculation/exploreTaxCalculation.js
+++ force-app/main/default/lwc/exploreTaxCalculation/exploreTaxCalculation.js
@@ -7,24 +7,16 @@ export default class ExploreTaxCalculation extends LightningElement {
     totalTaxAmount;
 
     handleChange(event){
-
-        console.log('=========>1');
-
+        
         const field = event.target.name;
-
-        console.log('=========>2');
-
         if(field === 'totalAmount'){
             this.totalAmount = event.target.value;  
-            console.log('=========>3');
         }
         if(field === 'taxPercentage'){
             this.taxPercentage = event.target.value;
-            console.log('=========>4');
         }
         if(this.totalAmount !== undefined && this.taxPercentage !== undefined){
             this.totalTaxAmount = this.template.querySelector('c-explore-math').calculate(this.totalAmount, this.taxPercentage);
-            
         }
     }
 
