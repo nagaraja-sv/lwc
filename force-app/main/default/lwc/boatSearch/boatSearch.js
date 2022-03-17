fc471f40713ab71269eed138c6716dcf948ae1c1
venkat <venkatanagarajas@gmail.com>
2022-03-17 13:37:59 +0530

    pushed debasis sessions code

---
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,30 @@
+// imports
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class BoatSearch extends NavigationMixin(LightningElement) {
+    isLoading = false;
+
+    // Handles loading event
+    handleLoading() { }
+
+    // Handles done loading event
+    handleDoneLoading() { }
+
+    // Handles search boat event
+    // This custom event comes from the form
+    searchBoats(event) { }
+
+    createNewBoat() {
+
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Boat__c',
+                actionName: 'new'
+            }
+        })
+
+
+    }
+}
